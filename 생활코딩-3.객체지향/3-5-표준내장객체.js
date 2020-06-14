5509a95fa675d8c84f3c9bbd48c2b951af58a494
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-14 14:01:26 +0900

    docs : create 3-5.js

---
+++ 생활코딩-3.객체지향/3-5-표준내장객체.js
@@ -0,0 +1,18 @@
+/*
+표준 내장 객체(Standard Built-in Object)는 
+js에서 아무리 못해도 기본으로 있는 객체들을 말한다. 
+이러한 내장 객체들은 프로그래밍을 하는데 기본 필요 도구들이며, 
+이것들과 +로 호스트 환경에서 제공하는 추가 기능들을 활용해서 소프트웨어를 만들기에, 이 내장객체를 잘 알아두는 것이 중요하다.
+
+js의 내장객체는,
+Object
+Function
+Array
+String
+Boolean
+Number
+Math
+Date
+RegExp
+가 있다.
+*/
