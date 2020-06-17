dce857a9a037931e34bf1511246892d79f2be41b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-17 19:10:42 +0900

    docs : finish update 3-6.js

--- 생활코딩-3.객체지향/3-6-object.js
+++ 생활코딩-3.객체지향/3-6-object.js
@@ -6,6 +6,7 @@ Object 객체란, 객체의 가장 기본적인 형태를 가진 객체로, 아�
 var nullObj1 = new Object()
 var nullObj2 = Object()
 var obj3 = Object(3)
+// 보통 빈객체를 Object() 대신 {}으로 생성한다.
 
 console.log(nullObj1)
 console.log(nullObj2)
@@ -18,5 +19,36 @@ console.log(obj3)
 참고로 생성자로 Object로 호출(new Object())하지 않고, 걍 호출(Object())해도 생성자로 호출한 것과 동일하게 동작한다.
 */
 
+console.log(Object.length)  // 1의 값 가짐
+console.log(Object.prototype)   // Object 형 모든 객체에 추가한 속성 값
+/*
+위같이 Object 객체는 다양한 속성과 메서드를 가지고 있다.
+- 자세한 내용 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object 참고
 
-// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object
+그리고 Object 객체를 확장해서, 모든 객체가 접근할 수 있는 API(기능, 메서드)를 만들 수 있다.
+*/
+
+Object.prototype.haha = function(n){    // Object 객체에 메서드를 추가(API 확장)
+    for(var yoso in this){
+        if(this[yoso] === n){
+            console.log('객체안에 넘겨준 값이 존재합니다!')
+            return true
+        }
+    }
+    console.log('객체안에 넘겨준 값이 존재하지 않습니다!')
+    return false
+}
+
+var ob1 = {'name':'반야', 'live': 'daejeon'}
+var ob2 = ['보리', '살타', '바라밀']
+console.log(ob1.haha('반야'))
+console.log(ob2.haha('반야'))
+/*
+위같이 말인데, 
+Object 객체를 확장하면 모든 객체에 영향을 주기에, 혼란을 만들 수 있어서 안 쓰는 것이 바람직하다.
+
+여기서 Object 객체에서 확장된 속성인지, 아님 원래 객체에 있던 속성인지 혼란스러울 때 구분할 방법으로는,
+hasOwnProperty() 가 있다.
+이 메서드의 인자값으로 속성명을 넘겨주면, 해당 속성명이 객체의 속성인지 아닌지를 true, false으로 반환한다.
+여기서 Object 객체에서 확장된 속성처럼 prototype으로 상속받은 객체는 false가 된다.
+*/
